95242bf734fe8b32797bc5c64f9f4ddc929a8229
Kartofanych <66062429+Kartofanych@users.noreply.github.com>
2021-04-16 18:38:40 +0300

    Karim/Test#1
    just_for_test

--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -9,6 +9,9 @@ public class Swipe : MonoBehaviour, IBeginDragHandler, IDragHandler
     public Transform player;
     public float dragSpeed = 0.1f;
     private float playerXBound = 4.5f;
+
+    //just_for_fun
+    private float playerYBound = 9f;
     
     public void OnBeginDrag(PointerEventData eventData) {
         
@@ -24,6 +27,17 @@ public class Swipe : MonoBehaviour, IBeginDragHandler, IDragHandler
         }
         if (player.position.x < -playerXBound) {
             player.position = new Vector3(-playerXBound, player.position.y, player.position.z);
+        }
+
+
+        //just_for_fun
+        player.Translate(Vector3.up * eventData.delta.y * dragSpeed);
+
+        if (player.position.y > playerYBound){
+            player.position = new Vector3(player.position.x, playerYBound, player.position.z);
+        }
+        if (player.position.y < -playerYBound){
+            player.position = new Vector3(player.position.x, -playerYBound, player.position.z);
         }
     }
 }
